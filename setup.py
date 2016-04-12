62f218844ba965dee2ec3954e4546f96a38f2c5b
pascal.bouchareine <pascal@gandi.net>
2016-04-11 17:59:00 -0700

    add debian packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+requires = [
+    'requests',
+    'dnspython',
+    'PySocks>=1.5.6',
+    'pycrypto'
+]
+
 setup(
     name = "dnsknife",
     version = "0.1",
     packages = find_packages(),
-    description = "DNS tools"
+    description = "DNS tools",
+    install_requires = requires
 )
